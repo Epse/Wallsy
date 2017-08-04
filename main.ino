a240bfca1aba2d78be1b2991996edf99f7b564a7
Stef Pletinck <s@stefpletinck.be>
2017-08-04 15:44:33 +0200

    motor framework

--- main.ino
+++ main.ino
@@ -8,9 +8,54 @@
 #DEFINE H-BRIDGE_TOP_ENABLE 15
 #DEFINE H-BRIDGE_TOP_A 14
 #DEFINE H-BRIDGE_TOP_B 5
-#DEFINE H-BRIDGE_MVMT_ENABLE 21
+#DEFINE H-BRIDGE_MVMT_ENABLE 8
 #DEFINE H-BRIDGE_MVMT_A 17
 #DEFINE H-BRIDGE_MVMT_B 16
+#DEFINE DISTANCE_SENS 20
+
+class Motor {
+private:
+  int enablePin;
+  int aPin;
+  int bPin;
+
+public:
+  Motor(initEnablePin, initAPin, initBPin) {
+    enablePin = initEnablePin;
+    aPin = initAPin;
+    bPin = initBPin;
+
+    pinMode(enablePin, OUTPUT);
+    pinMode(aPin, OUTPUT);
+    pinMode(bPin, OUTPUT);
+  }
+
+  void disable() {
+    digitalWrite(enablePin, LOW);
+  }
+
+  void enable() {
+    digitalWrite(enablePin, HIGH);
+  }
+
+  void forwards() {
+    digitalWrite(aPin, LOW);
+    digitalWrite(bPin, HIGH);
+  }
+
+  void backwards() {
+    digitalWrite(aPin, HIGH);
+    digitalWrite(bPin, LOW);
+  }
+
+  void stop() {
+    digitalWrite(aPin, LOW);
+    digitalWrite(bPin, LOW);
+  }
+}
+
+Motor headMotor;
+Motor mvmtMotor;
 
 void setup() {
   // All my outputs. Tons!!!
@@ -18,17 +63,16 @@ void setup() {
   pinMode(LEFT_EYE_LED, OUTPUT);
   pinMode(RIGHT_EYE_LED, OUTPUT);
   pinMode(SPEAKER, OUTPUT);
-  pinMode(H-BRIDGE_TOP_ENABLE, OUTPUT);
-  pinMode(H-BRIDGE_TOP_A, OUTPUT);
-  pinMode(H-BRIDGE_TOP_B, OUTPUT);
-  pinMode(H-BRIDGE_MVMT_ENABLE, OUTPUT);
-  pinMode(H-BRIDGE_MVMT_A, OUTPUT);
-  pinMode(H-BRIDGE_MVMT_B, OUTPUT);
+  pinMode(DISTANCE_SENS, INPUT);
 
   // Inputs. Wayyyy less.
   pinMode(FRONT_BTN, INPUT);
   pinMode(IR_TOP, INPUT);
   pinMode(IR_REAR, INPUT);
+
+  // Own libraries
+  headMotor.init(H-BRIDGE_TOP_ENABLE, H-BRIDGE_TOP_A, H-BRIDGE_TOP_B);
+  mvmtMotor.init(H-BRIDGE_MVMT_ENABLE, H-BRIDGE_MVMT_A, H-BRIDGE_MVMT_B);
 }
 
 void loop() {
