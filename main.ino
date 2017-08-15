b9da97b228c54addc73448bad270dd53134c8950
Stef Pletinck <s@stefpletinck.be>
2017-08-15 13:59:41 +0200

    Added dispatchers and simple status blinker

--- main.ino
+++ main.ino
@@ -18,6 +18,9 @@
 Motor headMotor;
 Motor mvmtMotor;
 
+unsigned long lastMillis;
+boolean statusLedValue;
+
 void setup() {
   // All my outputs. Tons!!!
   pinMode(STATUS_LED, OUTPUT);
@@ -32,9 +35,52 @@ void setup() {
   pinMode(IR_REAR, INPUT);
 
   // Own libraries
+  // This also initializes the necessary pins to output.
   headMotor.init(H_BRIDGE_TOP_ENABLE, H_BRIDGE_TOP_A, H_BRIDGE_TOP_B);
   mvmtMotor.init(H_BRIDGE_MVMT_ENABLE, H_BRIDGE_MVMT_A, H_BRIDGE_MVMT_B);
 }
 
+// Is called as fast as possible with the delta the millisecond difference since last call.
+// WARNING: the delta can be 0!
+void tickDispatch(unsigned long delta) {
+  if (digitalRead(FRONT_BTN)) {
+    //TODO: haven't got a clue what this is for.
+  }
+  // IR code should come here, too.
+}
+
+void 5MillisDispatch(unsigned long delta) {
+  // placeholder
+}
+
+void 10MillisDispatch(unsigned long delta) {
+  //placeholder
+}
+
+void halfSecondDispatch(unsigned long delta) {
+  digitalWrite(STATUS_LED, !statusLedValue);
+}
+
+
+
+// This is a dispatch function.
+// NOTHING should ever by directly here. Everything MUST be in a dispatch function.
 void loop() {
+  // the delta can't underflow since time travel hasn't been invented yet...
+  tickDispatch(Millis() - lastMillis);
+
+  // This code WILL break after about 50 days due to `Millis()` overflowing. Not an issue.
+  // Additional dispatches may be added if necessary
+  if (Millis() >= lastMillis + 5) {
+    5MillisDispatch(Millis() - lastMillis);
+  }
+  if (Millis >= lastMillis + 10) {
+    10MillisDispatch(Millis() - lastMillis);
+  }
+  if (Millis >= lastMillis + 500) {
+    halfSecondDispatch(Millis() - lastMillis);
+  }
+
+  // I think this is obvious
+  lastMillis = Millis();
 }
